66644af30ee869b6c602c80231aee92623b67718
16CO142 <shreyaspandith1210@gmail.com>
2018-10-17 11:20:49 +0530

    starting authentication

--- cms/app.js
+++ cms/app.js
@@ -1,8 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
+var mysql = require('mysql');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +14,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(logger('dev'));
 app.use(express.json());
--- cms/routes/users.js
+++ cms/routes/users.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var connection=require('../db');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/signup', function(req, res, next) {
+  // console.log("req",req.body);
+  var today = new Date();
+  var user={
+   
+    "mail":req.body.email,
+    "password":req.body.password,
+    
+  }
+  connection.query('INSERT INTO STU SET ?',user, function (error, results, fields) {
+  if (error) {
+    console.log("error ocurred",error);
+    res.send({
+      "code":400,
+      "failed":"error ocurred"
+    })
+  }else{
+    console.log('The solution is: ', results);
+    res.send({
+      "code":200,
+      "success":"user registered sucessfully"
+        });
+  }
+  });
 });
+
+router.get('/signup', function(req, res, next) {
+  console.log("not    not not  kdl");
+  connection.query('SELECT * FROM us limit 2',function(err,rows,fields){
+    res.render('users/all',{data: rows});
+});
+
+});
+router.get('/', function(req, res, next) {
+  connection.query('SELECT * FROM us',function(err,rows,fields){
+    res.render('users/all',{data: rows});
+
+});
+});
+
+
+
+
+
 
 module.exports = router;
